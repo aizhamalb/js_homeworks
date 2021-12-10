b9181b645c0f797acfa93a0da00819528d2a774c
Aizhamal Borisova <32429935+aizhamalb@users.noreply.github.com>
2021-12-10 17:06:50 +0600

    fixed

--- Task0019/script.js
+++ Task0019/script.js
@@ -1,4 +1,4 @@
 let a = Number(prompt("Введите ширину (в метрах):"));
 let b = Number(prompt("Введите высоту (в метрах):"));
-let S = a * b
-console.log ("Площадь стены" + (S) + "м2")
+let s = a * b
+console.log ("Площадь стены" + s + "м2")
